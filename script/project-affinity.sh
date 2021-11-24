b9db785996d3a198603d021a12004d4bded0a5c1
bkil <b.K.il.h.u+tigbuh@gmail.com>
2021-11-24 11:04:51 +0100

    script/project-affinity: dynamically spread all projects between cores

--- script/project-affinity.sh
+++ script/project-affinity.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
-while true; do
-  pgrep --full ../../projects/www.worldcommunitygrid.org/ |
-  while read PID do
-    taskset --all-tasks --pid 0x55555555 $PID
-  done
-
-  pgrep --full ../../projects/boinc.bakerlab.org_rosetta/ |
+poll() {
+  pgrep --full "^/usr/bin/boinc " |
+  xargs ps u --ppid |
+  tail -n +2 |
+  tr -s " " |
+  cut -d " " -f 2,11 |
+  sed -r "s~^(.*) (.*)$~\2 \1~" |
+  sort |
+  cut -d " " -f 2 |
   while read PID; do
-    taskset --all-tasks --pid 0xAAAAAAAA $PID
-  done
+    [ -n "$N" ] || N=0
 
-  sleep 60
-done
+    taskset --all-tasks --pid --cpu-list $N $PID
+
+    N=`expr $N + 2`
+    [ "$N" -eq "$VCPU" ] && N="1"
+    [ "$N" -gt "$VCPU" ] && N="0"
+  done
+}
+
+main() {
+  VCPU="`grep -c "^processor" /proc/cpuinfo`"
+  while true; do
+    poll
+    sleep 60
+  done
+}
+
+main "$@"
